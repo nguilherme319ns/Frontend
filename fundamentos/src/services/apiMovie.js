73874517c4bd633814c2b8f446f705a096fefa90
nguilherme319ns <nguilherme319@gmail.com>
2024-09-09 15:18:00 -0300

    Atividade5 09/09

--- fundamentos/src/services/apiMovie.js
+++ fundamentos/src/services/apiMovie.js
@@ -6,8 +6,8 @@ const apiMovie = axios.create({
         language: "pt-BR"
     },
     headers: {
-        Authorization: 'Bearer ' + process.env.API_KEY
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmVhOWI2ZDliODA3M2Q2ZmVlZDg0ZWMzNjNjYTVkMyIsIm5iZiI6MTcyNTkwNTgzMC41MDQyNzgsInN1YiI6IjY2ZDlhMTEyNDAxM2VlYTRlYTkyODMzYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EPVl3CkYr7tY_VOxM6KTWF2orTJEq-4ybQKP8HBrTVQ ' + process.env.API_KEY
     }
-})
+})  
 
 export default apiMovie
